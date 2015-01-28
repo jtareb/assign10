a7f5b41edd395e9184e94bdea72ce2135c89a035
John Albritton <jtareb1@att.net>
2015-01-28 08:12:15 -0500

    final commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,23 @@
 
+ //_each(user_items, function() {
+
+
+
+//var pic = user_items.avatar_url;
+   
+//var templateString = $('#item-user-template').text();
+
+    // Turn it into a template function.
+    //var renderTemplate = _.template(templateString);
+
+    // Pass in an object. Return value is a string
+    // with the bee stings replaced with object's properties
+    //var freshHTML = renderTemplate(p);
+
+    // Inject the fresh html into the page.
+   // $('.sidebar').append(freshHTML);//
+
+
 
 
 
@@ -14,6 +33,7 @@ _.each(repo_items,function(p) {
 
     var classwork = p.name;
 	var desc = p.description;
+    
 
 
 	//console.log (desc);
