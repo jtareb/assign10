5e3f9929fa00bacf75233e06657fd6667284ef50
John Albritton <jtareb1@att.net>
2015-03-06 18:23:55 -0500

    debug

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,29 +1,20 @@
+;(function() {
+ 
 
- //_each(user_items, function() {
 
+ _.each(user_items, function() {
 
+       
 
-//var pic = user_items.avatar_url;
+    //var pic = user_items.avatar_url;
    
-//var templateString = $('#item-user-template').text();
+    var templateString = $('#item-user-template').text();
 
-    // Turn it into a template function.
-    //var renderTemplate = _.template(templateString);
-
-    // Pass in an object. Return value is a string
-    // with the bee stings replaced with object's properties
-    //var freshHTML = renderTemplate(p);
-
-    // Inject the fresh html into the page.
-   // $('.sidebar').append(freshHTML);//
-
-
-
-
-
-
-
-
+    var renderTemplate = _.template(templateString);
+   
+    var freshHTML = renderTemplate(user_items.avatar_url);
+   
+     $('.sidebar').append(freshHTML);
 
 
 
@@ -33,31 +24,23 @@ _.each(repo_items,function(p) {
 
     var classwork = p.name;
 	var desc = p.description;
-    
 
-
-	//console.log (desc);
-	console.log(p);
-
-
-
-
-
- // Grab the template string.
     var templateString = $('#item-user-template').text();
 
-    // Turn it into a template function.
+    
     var renderTemplate = _.template(templateString);
 
-    // Pass in an object. Return value is a string
-    // with the bee stings replaced with object's properties
     var freshHTML = renderTemplate(p);
-
-    // Inject the fresh html into the page.
+   
     $('.contentbox').append(freshHTML);
 
-
-
-
+    });
 
 });
+
+
+
+}());
+
+
+
