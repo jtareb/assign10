5c6c712dd182741a3924475c0c95dda8b0e746b3
John Albritton <jtareb1@att.net>
2015-04-27 22:15:30 -0400

    fix

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,46 +1,48 @@
-;(function() {
- 
 
 
- _.each(user_items, function() {
+var avatar = '<img src="' + userAPI.avatar_url + '">';
+var userFullName = userAPI.name;
+var userLoginName = userAPI.login;
+var joined = userAPI.created_at;
+var followersList = userAPI.followers_url;
+var starredList = userAPI.starred_url;
+var followingList = userAPI.following_url;
+var numFollowers = userAPI.followers;
+var templateSideBar = $('#userTemplate').text();
+var starredNum = starredAPI.length;
+var renderUserTemplate = _.template(templateSideBar);
 
+var userHTML = renderUserTemplate({starredNumber: starredNum,numFollower: numFollowers,followingURL: followingList,userAvatar: avatar, userName: userFullName, userLogin: userLoginName, joinedDate: joined,followURL: followersList,starredURL: starredList});
+
+$('.sideBar').append(userHTML);
+
+
+
+
+_.each(repoAPI, function (p) {
+        console.log(p);
        
+        var repoLinks =  '<a href="' + p.html_url + '">'+ p.name +'</a><br/>';
+        //var repoDescription = 
+        var repoDescript = p.description;
 
-    //var pic = user_items.avatar_url;
-   
-    var templateString = $('#item-user-template').text();
+        var lastUpdate = p.updated_at;
 
-    var renderTemplate = _.template(templateString);
-   
-    var freshHTML = renderTemplate(user_items.avatar_url);
-   
-     $('.sidebar').append(freshHTML);
+    // Grab the template string.
+    var templateString = $('#repoTemplate').text();
 
-
-
-
-_.each(repo_items,function(p) {
-		
-
-    var classwork = p.name;
-	var desc = p.description;
-
-    var templateString = $('#item-user-template').text();
-
-    
+    // Turn it into a template function.
     var renderTemplate = _.template(templateString);
 
-    var freshHTML = renderTemplate(p);
-   
-    $('.contentbox').append(freshHTML);
+    // Pass in an object. Return value is a string
+    // with the bee stings replaced with object's properties
+    var freshHTML = renderTemplate({repoLink: repoLinks, descript: repoDescript, lastUpdated: lastUpdate});
 
-    });
-
+    // Inject the fresh html into the page.
+    $('.innerMain').append(freshHTML);
+ 
 });
 
-
-
-}());
-
+    
 
 
