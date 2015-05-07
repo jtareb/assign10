8a2341fad4526f60cacdcb0e25c24e1002ec53da
John Albritton <jtareb1@att.net>
2015-05-07 08:24:24 -0400

    fix

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,9 @@
-// Grab the left quarter container class, append user data into the user-template ID, do this for the "user" object.
+//build sidebar with the user template by appending data from user.js
 $('.sideBar').append(_.template($('#user-template').text())(user));
-
+//build sidebar with the org templage by appending data from org.js
 $('.sideBar').append(_.template($('#org-template').text())(org));
 
-
+//build repo section by appending data from repos.js
 repos.forEach(function (repo) {
 $('.content-container').append(_.template($('#repo-list-template').text())(repo));
 });
